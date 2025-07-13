cf9bcfd2914f9b96ef52f5742a7d38856170c8c8
amelielinha <amelie@linha.de>
2025-07-13 23:05:43 +0200

    translated code to English and added global variable clicked_coord as a dictionary

--- src/semikite/main.py
+++ src/semikite/main.py
@@ -1,29 +1,31 @@
 import matplotlib
-matplotlib.use('TkAgg')  # <- wichtig für GUI-Fenster
+matplotlib.use('TkAgg')  # <- important command for GUI window
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-img = mpimg.imread('FE4_Image_20160901_115500_UTCp1.jpg')
+image_paths = ['FE4_Image_20160901_115500_UTCp1.jpg']
 
-clicked_coord = None
+clicked_coord = []
 
 def onclick(event):
     global clicked_coord
     if event.xdata is not None and event.ydata is not None:
         x = int(event.xdata)
         y = int(event.ydata)
-        clicked_coord = (x, y)
-        print(f"Geklickt bei: x={x}, y={y}")
+        clicked_coord.append((x,y))
+        print(f"Coordinates: x={x}, y={y}")
         plt.close()
 
-fig, ax = plt.subplots()
-ax.imshow(img)
-fig.canvas.mpl_connect('button_press_event', onclick)
-plt.title("Bitte ins Bild klicken")
-plt.show()
+for path in image_paths:
+    img = mpimg.imread(path)
+    fig, ax = plt.subplots(figsize=(15,25))
+    ax.imshow(img)
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    plt.title("Please click on the kite in the image")
+    plt.show()
 
 if clicked_coord:
-    print("Gespeicherte Koordinaten:", clicked_coord)
+    print("Saved coordinates:", clicked_coord)
 else:
-    print("Kein Klick erkannt.")
+    print("It did not work.")
