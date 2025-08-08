cabf206e596d0124c88244380d52d9f91ae6eedc
bohong <bohongli2001@gmail.com>
2025-08-08 14:29:01 +0200

    opened a new file angle_calculation.py that dedicates to calculating the angle between the kite and the fisheye camera given coordinates in the output from auto_kite_detection

---
+++ src/autokite/angle_calculation.py
@@ -0,0 +1,11 @@
+"""
+    Author: Bohong Li
+
+    Given the coordinates found in auto_kite_detection.py, this script should calculate the angle of the kite
+    relative to the fisheye camera.
+"""
+
+import pandas as pd
+
+from functions_autokite import pixel_to_sky_angles
+
