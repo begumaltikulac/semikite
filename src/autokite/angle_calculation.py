0ce285eb4b4337c189d2bfd55ed13549c2148494
bohong <bohongli2001@gmail.com>
2025-08-08 17:07:51 +0200

    the first version of the algorithm that calculates the angle based on the pixel. takes the .pckl file with information about the pixel. the output returns a .csv file with elevation and azimuth angles.

--- src/autokite/angle_calculation.py
+++ src/autokite/angle_calculation.py
@@ -5,7 +5,22 @@
     relative to the fisheye camera.
 """
 
-import pandas as pd
+import pickle
 
 from functions_autokite import pixel_to_sky_angles
 
+with open("coordinates.pckl", "rb") as f:
+    coordinates = pickle.load(f)
+
+all_elevation = []
+all_azimuth = []
+
+for coord in coordinates["coordinates"]:
+    elevation, azimuth = pixel_to_sky_angles(coord[0], coord[1], 960, 960, 960)
+    all_elevation.append(int(round(elevation,0)))
+    all_azimuth.append(int(round(azimuth,0)))
+
+coordinates["elevation"] = all_elevation
+coordinates["azimuth"] = all_azimuth
+
+coordinates.to_csv("coordinates_with_angles.csv")
