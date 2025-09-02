ffb891fe9b7132cb3d50bd81be2543e93d247e38
bohong <bohongli2001@gmail.com>
2025-09-02 13:54:55 +0200

    rearranged and restructuring the codes to improve the readability

--- src/autokite/angle_calculation.py
+++ src/autokite/angle_calculation.py
@@ -9,14 +9,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
-from functions_autokite import pixel_to_sky_angles, pixel_to_angles_with_height
+from functions_autokite import (
+    open_theodolite,
+    pixel_to_sky_angles,
+    # pixel_to_angles_with_height
+)
+DATE = "20250829"
+coords_outfile = f"coordinates/coordinates_{DATE}.pckl"
+outcsv_name = f"coordinates/coordinates_with_angles_{DATE}.csv"
+theo_file = "TheoGelb_20250829_124013.txt"
 
-with open("coordinates/coordinates_20250829.pckl", "rb") as f:
-    coordinates = pickle.load(f)
+SUBFOLDER = "theo_with_no_radiosonde_first_flight"  # "theo_with_no_radiosonde_first_flight" "theo_with_radiosonde_second_flight"
+# coords_outfile = f"coordinates/coordinates_{DATE}_{SUBFOLDER}.pckl"
+# outcsv_name = f"coordinates/coordinates_with_angles_{DATE}_{SUBFOLDER}.csv"
 
 all_elevation = []
 all_azimuth = []
 heights = np.arange(40,100) # NOTE: The height information is still missing. We need that information from the radiosondes
+
+with open(coords_outfile, "rb") as f:
+    coordinates = pickle.load(f)
 
 # for coord, height in zip(coordinates["coordinates [x,y]"], heights):
 for coord in coordinates["coordinates [x,y]"]:
@@ -26,5 +38,14 @@ for coord in coordinates["coordinates [x,y]"]:
 
 coordinates["elevation"] = all_elevation
 coordinates["azimuth"] = all_azimuth
+# coordinates.to_csv(outcsv_name)
 
-coordinates.to_csv("coordinates/coordinates_with_angles_20250829.csv")
+theo = open_theodolite(file="theodolite_data/TheoGelb_20250829_124013.txt", obs_date="2025-08-29", start_time="10:31:08")
+theo_elevation = theo["elevation"]
+autokite_elevation = coordinates["elevation"]
+
+plt.figure(dpi=150)
+plt.plot(theo_elevation, label="yellow theodolite")
+plt.plot(autokite_elevation, label="autokite")
+plt.legend()
+plt.show()
