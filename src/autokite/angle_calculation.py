6cf6108f000604c8224ffb4e26b7d51802e5be84
bohong <bohongli2001@gmail.com>
2025-09-04 12:11:51 +0200

    adapted the code for 20250903. generalized the code structure

--- src/autokite/angle_calculation.py
+++ src/autokite/angle_calculation.py
@@ -15,10 +15,20 @@ from functions_autokite import (
 DATE = "20250903"
 theo_start_date = "2025-09-03"
 
-# SUBFOLDER = "theo_with_no_radiosonde_first_flight"
-SUBFOLDER = "theo_with_radiosonde_second_flight"
-coords_outfile = f"coordinates/{DATE}/coordinates_{DATE}_{SUBFOLDER}.pckl"
-outcsv_name = f"coordinates/{DATE}/coordinates_with_angles_{DATE}_{SUBFOLDER}.csv"
+for SUBFOLDER in ["morning","afternoon"]:
+    all_elevation = []
+    all_azimuth = []
+    coords_outfile = f"coordinates/{DATE}/coordinates_{DATE}_{SUBFOLDER}.pckl"
+    outcsv_name = f"coordinates/{DATE}/coordinates_with_angles_{DATE}_{SUBFOLDER}.csv"
+    with open(coords_outfile, "rb") as f:
+        coordinates = pickle.load(f)
+    for coord in coordinates["coordinates [x,y]"]:
+        elevation, azimuth = pixel_to_sky_angles(coord[0], coord[1])
+        all_elevation.append(int(round(elevation,0)))
+        all_azimuth.append(int(round(azimuth,0)))
+    coordinates["elevation"] = all_elevation
+    coordinates["azimuth"] = all_azimuth
+    coordinates.to_csv(outcsv_name)
 
 for theo_start_time, theo_file, color in zip(
     # ["10:03:20", "10:03:20"],
