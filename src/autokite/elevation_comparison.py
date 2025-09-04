0679afe4857b8d79bf6d50727665595d3fb4930f
bohong <bohongli2001@gmail.com>
2025-09-04 16:17:41 +0200

    improvement of autokite output with semikite now possible. plotting now also possible, alongside outputs of deviation in mean elevation angles, rmse and correlation between graphs

--- src/autokite/elevation_comparison.py
+++ src/autokite/elevation_comparison.py
@@ -1,14 +1,40 @@
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import pickle
 
-# matplotlib.use("Qt5Agg")  # "TkAgg"
+from functions_autokite import pixel_to_sky_angles
 
-DATE = "20250903"
-SUBFOLDER = "afternoon"
-theodolite = pd.read_csv(f'coordinates/{DATE}/theodolite_angles_20250903_{SUBFOLDER}.csv')[::2].reset_index(drop=True)
+matplotlib.use("Qt5Agg")  # "TkAgg"
+
+DATE = "20250901"
+SUBFOLDER = "morning"
+semikite_filename = f"coordinates/{DATE}/coordinates_semikite_20250901_morning.pckl"
+
+theodolite = pd.read_csv(f'coordinates/{DATE}/yellow_theodolite_angles_{DATE}_{SUBFOLDER}.csv')[::2].reset_index(drop=True)
 theo_elevation = theodolite["elevation"]
-autokite = pd.read_csv(f'coordinates/{DATE}/coordinates_with_angles_20250903_{SUBFOLDER}.csv')
+autokite = pd.read_csv(f'coordinates/{DATE}/coordinates_with_angles_{DATE}_{SUBFOLDER}.csv')
+autokite.set_index('time', inplace=True)
+
+with open(semikite_filename, "rb") as f:
+    semikite = pickle.load(f)
+semikite_all_elevation = []
+semikite_all_azimuth = []
+semikite_coordinates = semikite["coordinates [x,y]"]
+for coord in semikite_coordinates:
+    elevation, azimuth = pixel_to_sky_angles(coord[0], coord[1])
+    semikite_all_elevation.append(int(round(elevation, 0)))
+    semikite_all_azimuth.append(int(round(azimuth, 0)))
+semikite["elevation"] = semikite_all_elevation
+semikite["azimuth"] = semikite_all_azimuth
+
+autokite.update(semikite)
+autokite.reset_index(inplace=True)
 autokite_elevation = autokite["elevation"]
+
+mean_diff = abs(np.mean(theo_elevation) - np.mean(autokite_elevation))
+rmse = np.sqrt(((theo_elevation-autokite_elevation)**2).mean())
 
 plt.figure(dpi=150)
 plt.plot(theo_elevation, label="yellow theodolite")
@@ -16,3 +42,7 @@ plt.plot(autokite_elevation, label="autokite")
 plt.legend()
 plt.title(f"elevation comparison of launch {DATE} {SUBFOLDER}")
 plt.show()
+
+print(f"The mean angle difference is: {mean_diff}")
+print(f"The rmse is {rmse}.")
+print(f"The correlation between two measurement is: {theo_elevation.corr(autokite_elevation)}")
